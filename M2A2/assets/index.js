dcdd283399f09e54d3dd653477fd8aa7f2c987ef
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2025-12-08 21:37:22 -0500

    M6A1

---
+++ M2A2/assets/index.js
@@ -0,0 +1,5 @@
+export const header = require("./starwars1.jpg");
+export const icon = require("./icon.png");
+export const splash = require("./splash-icon.png");
+export const adaptiveIcon = require("./adaptive-icon.png");
+export const favicon = require("./favicon.png");
